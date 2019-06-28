81bee4a1b5d3c61258be2505981a305448f352d8
Rey Felipe <jrmfelipe@gmail.com>
2019-06-28 16:32:14 +0800

    Handle Search TextField and Search Button

--- iTunesMusicSearch/ViewController.swift
+++ iTunesMusicSearch/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    let session = URLSession.shared
+    let url = URL(string: "https://itunes.apple.com/search?media=music&term=work")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - Actions
+    @IBAction func searchButtonTapped() {
+        
+        print("invoke search here....")
+        
+    }
 
-
+    //MARK: - UITextFieldDelegate
+    func textField(_ textFieldToChange: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        searchButton.isEnabled = !string.isEmpty
+        
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        if !textField.text!.isEmpty {
+            searchButtonTapped()
+            return true
+        }
+        return false
+    }
+ 
 }
 
